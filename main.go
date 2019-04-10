b960556eeb6a6d6f4ddff630c9b2bf6cfd1a9013
leedy <yulidaiahx@163.com>
2019-04-10 18:09:11 +0800

    add client page

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 func main() {
 	hub := newHub()
 	go hub.run()
-	
+
+	http.Handle("/", http.FileServer(http.Dir("./client")))
 	http.HandleFunc("/ws", hub.handler)
 	http.ListenAndServe("localhost:9191", nil)
 }
